6690314ab09fac0bde4db67ad1a2db6eb702a3e1
David Durieux <d.durieux@siprossii.com>
2016-03-13 00:02:02 +0100

    Update unit tests

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -1,11 +1,11 @@
 <?php
 
-include('../src/Client.php');
+include('src/Client.php');
 
 class LoginTest extends PHPUnit_Framework_TestCase {
 
 
-    public function testLogin(Successful) {
+    public function testLoginSuccessful() {
 
         $abc = new Alignak_Backend_Client('http://127.0.0.1:5000/');
         $abc->login('admin', 'admin');
